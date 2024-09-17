bfc6b20eec23adc9d82d73b3445b76286b32dda8
Divith102 <u1482564@umail.utah.edu>
2024-09-17 10:44:42 -0600

    camfix

--- Assets/Scripts/Camera/CamMovement.cs
+++ Assets/Scripts/Camera/CamMovement.cs
@@ -1,21 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CamMovement : MonoBehaviour
 {
     public float followSpeed = 3f;
     public Transform target;
-    [SerializeField] int[] camSize ={8,7,6,5,4};
+    private float[] camSize ={8,7,6,5,4};
     private int zoom;
-
+    static float t = 0.0f;
     Camera cam;
     public GameObject Player;
     // Start is called before the first frame update
     void Start()
     {
         cam = GetComponent<Camera>();
-
     }
 
     // Update is called once per frame
@@ -24,7 +24,10 @@ public class CamMovement : MonoBehaviour
         float stressLevel = Player.gameObject.GetComponent<PlayerMovements>().stressLevel;
         Vector3 newPos= new Vector3(target.position.x,target.position.y,-2);
         transform.position = Vector3.Slerp(transform.position,newPos,followSpeed*Time.deltaTime);
-        Debug.Log(stressLevel);
+        if (t > 1.0f)
+        {
+            t = 0.0f;
+        }
         if (stressLevel <= 20)
         {
             zoom = 1;
@@ -33,22 +36,26 @@ public class CamMovement : MonoBehaviour
         else if (stressLevel > 20 && stressLevel <= 40)
         {
             zoom = 2;
-            cam.orthographicSize = Mathf.Clamp(cam.orthographicSize - 0.1f * Time.deltaTime, camSize[0], camSize[1]);
+            //cam.orthographicSize = Mathf.Lerp(camSize[0], camSize[1],t);
+            //t += 0.5f * Time.deltaTime;
+            cam.orthographicSize = camSize[2];
         }
         else if (stressLevel > 40 && stressLevel <= 60)
         {
             zoom = 3;
-            cam.orthographicSize = Mathf.Clamp(cam.orthographicSize - 0.1f * Time.deltaTime, camSize[1], camSize[2]);
+            //cam.orthographicSize = Mathf.Lerp(camSize[1], camSize[2], t);
+            //t += 0.5f * Time.deltaTime;
+            cam.orthographicSize = camSize[2];  
         }
         else if (stressLevel > 60 && stressLevel <= 80)
         {
             zoom = 4;
-            cam.orthographicSize = Mathf.Clamp(cam.orthographicSize - 0.1f * Time.deltaTime, camSize[2], camSize[3]);
+            cam.orthographicSize = camSize[3];
         }
         else if (stressLevel > 80 && stressLevel <=99)
         {
             zoom = 5;
-            cam.orthographicSize = Mathf.Clamp(cam.orthographicSize - 0.1f * Time.deltaTime, camSize[3], camSize[4]);
+            cam.orthographicSize = camSize[4];
         }
 
     }
