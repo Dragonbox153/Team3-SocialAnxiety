e773eed991c37f97d4308d3728cba49b26f80089
Divith102 <u1482564@umail.utah.edu>
2024-09-17 19:36:55 -0600

    camfix

--- Assets/Scripts/Camera/CamMovement.cs
+++ Assets/Scripts/Camera/CamMovement.cs
@@ -8,98 +8,67 @@ public class CamMovement : MonoBehaviour
 {
     public float followSpeed = 3f;
     public Transform target;
-    private float[] camSize ={8,7,6,5,4};
+    private float[] camSize ={8,6,4,2,1};
     static float t = 0.0f;
     Camera cam;
     public GameObject Player;
-
-    private int variableValue;
-    private int isIncreasing=0;
-
+    private int zoom;
+    private int oldZoom;
     // Start is called before the first frame update
     void Start()
     {
         cam = GetComponent<Camera>();
+        oldZoom = 1;
     }
 
-    public int zoom
-    {
-        get
-        {
-            return variableValue;
-        }
-        set
-        {
-            if (value > variableValue)
-            {
-                isIncreasing = 1;
-            }
-            else if (value < variableValue)
-            {
-                isIncreasing = -1;
-            }
-            else
-            {
-                isIncreasing = 0;
-            }
-            variableValue = value;
-            //isIncreasing = variableValue <= value;
-            //variableValue = value;
-        }
-    }
 
 
     // Update is called once per frame
     void Update()
     {
-
+        // camera tracking the player and moving
         Vector3 newPos= new Vector3(target.position.x,target.position.y,-2);
         transform.position = Vector3.Slerp(transform.position,newPos,followSpeed*Time.deltaTime);
 
-
+        // getting value of stress from player script
         float stressLevel = Player.gameObject.GetComponent<PlayerMovements>().stressLevel;
-        if (t > 1.0f)
+
+        if (zoom != oldZoom)
         {
             t = 0.0f;
+            oldZoom = zoom; // Update previous zoom to the current one
         }
+
+        // Chainging camera size
         if (stressLevel <= 20)
         {
             zoom = 1;
-            //cam.orthographicSize = camSize[0];
+            cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, camSize[0], t);
+            t += 0.01f * Time.deltaTime;
         }
         else if (stressLevel > 20 && stressLevel <= 40)
         {
             zoom = 2;
-            //cam.orthographicSize = Mathf.Lerp(camSize[0], camSize[1],t);
-            //t += 0.5f * Time.deltaTime;
-            //cam.orthographicSize = camSize[1];
+            cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, camSize[1], t);
+            t += 0.01f * Time.deltaTime;
         }
         else if (stressLevel > 40 && stressLevel <= 60)
         {
             zoom = 3;
-            //cam.orthographicSize = Mathf.Lerp(camSize[1], camSize[2], t);
-            //t += 0.5f * Time.deltaTime;
-            //cam.orthographicSize = camSize[2];  
+            cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, camSize[2], t);
+            t += 0.01f * Time.deltaTime;
         }
         else if (stressLevel > 60 && stressLevel <= 80)
         {
             zoom = 4;
-            //cam.orthographicSize = camSize[3];
+            cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, camSize[3], t);
+            t += 0.01f * Time.deltaTime;
         }
         else if (stressLevel > 80 && stressLevel <=99)
         {
             zoom = 5;
-            //cam.orthographicSize = camSize[4];
-        }
-        Debug.Log(isIncreasing);
-        if(isIncreasing ==0)
-        {
-
-        }
-        else if (isIncreasing > 0 && zoom>=2)
-        {
-            cam.orthographicSize = Mathf.Lerp(camSize[zoom-2], camSize[zoom-1],t);
-            t += 0.5f * Time.deltaTime;
+            cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, camSize[4], t);
+            t += 0.01f * Time.deltaTime;
         }
     }
 }
