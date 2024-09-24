7792edd70bebfbd6754e864b5bc461c9bff634c4
Dragonbox153 <u1531795@umail.utah.edu>
2024-09-24 09:34:47 -0600

    Started reworking enemySightCollision

--- Assets/Scripts/Enemy/EnemySightCollision.cs
+++ Assets/Scripts/Enemy/EnemySightCollision.cs
@@ -6,13 +6,27 @@ public class EnemySightCollision : MonoBehaviour
 {
     [SerializeField] int stressIncrease = 2;
     [SerializeField] float timeToIncrease = 0.5f;
+    [SerializeField] PlayerMovements player;
+    private bool onred;
     float time = 0;
-    private void OnTriggerStay2D(Collider2D collision)
+    private void OnTriggerEntry2D(Collider2D collision)
+    {
+        
+        if (collision.tag == "Player")
+        {
+            onred = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        onred = false;
+    }
+    private void Update()
     {
         time += Time.deltaTime;
-        if (collision.tag == "Player" && time >= timeToIncrease)
+        if (onred)
         {
-            collision.gameObject.GetComponent<PlayerMovements>().stressLevel += stressIncrease;
+            player.gameObject.GetComponent<PlayerMovements>().stressLevel += stressIncrease;
             time = 0;
         }
     }
