ccf7e102c48c44506bf834dc9ca4833aa9b0e014
Divith102 <u1482564@umail.utah.edu>
2024-10-02 14:00:26 -0600

    some

--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -18,7 +18,7 @@ public class EnemyMovement : MonoBehaviour
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
         if (transform.position != movementPoints[nextPoint].transform.position)
         {
