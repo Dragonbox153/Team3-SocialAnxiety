ec71e4974cfac86352de603f1a0448dd450baf66
Divith102 <u1482564@umail.utah.edu>
2024-10-03 09:44:55 -0600

    bug fix

--- Assets/Scripts/Camera/CamMovement.cs
+++ Assets/Scripts/Camera/CamMovement.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Rendering.PostProcessing;
-using static UnityEditor.Experimental.AssetDatabaseExperimental.AssetDatabaseCounters;
 
 public class CamMovement : MonoBehaviour
 {
